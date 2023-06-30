523acbed6f5b377d1a275bbd762760f60534a673
Egoh <95_grozn_95@mail.ru>
2023-06-30 15:00:41 +0600

    trubi

---
+++ script.js
@@ -0,0 +1,96 @@
+const data = [
+  {
+    image: "./img/nerzhlist.jpg",
+    title: "Листы стальные",
+    description:
+      "Lorem ipsum dolor sit consectetur adipiscing elit. Viverra in ul",
+    link: "Расчитать заказ",
+  },
+	{
+    image: "./img/shveller-1.jpg",
+    title: "Швеллера",
+    description:
+      "Lorem ipsum dolor sit consectetur adipiscing elit. Viverra in ul",
+    link: "Расчитать заказ",
+  },
+  {
+    image: "./img/126.0x460@2x.jpg",
+    title: "Трубы профильные",
+    description:
+      "Lorem ipsum dolor sit consectetur adipiscing elit. Viverra in ul",
+    link: "Расчитать заказ",
+  },
+  {
+    image: "./img/armatura_aIII (400)_v.jpg",
+    title: "Арматура",
+    description:
+      "Lorem ipsum dolor sit consectetur adipiscing elit. Viverra in ul",
+    link: "Расчитать заказ",
+  },
+	{
+    image: "./img/nerzhaveyushie-listy_DXPp7dq.jpg",
+    title: "Нерж.листы",
+    description:
+      "Lorem ipsum dolor sit consectetur adipiscing elit. Viverra in ul",
+    link: "Расчитать заказ",
+  },
+	{
+    image: "./img/truba1.jpeg",
+    title: "Трубы кругл. стальные",
+    description:
+      "Lorem ipsum dolor sit consectetur adipiscing elit. Viverra in ul",
+    link: "Расчитать заказ",
+  },
+  {
+    image: "./img/dvutavr.jpg",
+    title: "Двутавр",
+    description:
+      "Lorem ipsum dolor sit consectetur adipiscing elit. Viverra in ul",
+    link: "Расчитать заказ",
+  },
+  {
+    image: "./img/nerzhtrub.jpg",
+    title: "Long Lasting",
+    description:
+      "Lorem ipsum dolor sit consectetur adipiscing elit. Viverra in ul",
+    link: "Расчитать заказ",
+  },
+];
+
+const parentElement = document.getElementById("parent");
+
+const blocks = data.map((item) => {
+  const block = document.createElement("div");
+  block.className = "block";
+
+	
+  const image = document.createElement("img");
+  image.className = "image";
+  image.textContent = item.image;
+  image.src = item.image;
+	
+  const title = document.createElement("h2");
+  title.className = "title";
+  title.textContent = item.title;
+	
+  const description = document.createElement("p");
+  description.className = "description";
+  description.textContent = item.description;
+	
+	const link = document.createElement("a");
+	link.className = "link";
+  link.textContent = item.link;
+	link.href = '#';
+
+  block.appendChild(image);
+  block.appendChild(title);
+  block.appendChild(description);
+	block.appendChild(link);
+
+
+  return block;
+});
+
+blocks.forEach((block) => {
+  parentElement.appendChild(block);
+});
